70b89959ea95114552265b0a68ec8156c8708d2f
Surafel-Alayou <surafel2alayou@gmail.com>
2023-07-22 07:01:42 -0700

    First review

--- _printf.c
+++ _printf.c
@@ -1,41 +1,30 @@
 #include "main.h"
 
 /**
- * check - check for specfier
- * @c: char to check
- * Return: 1 if true 0 if false
- */
-int check(char c)
-{
-	if (c == 'c' || c == 's' || c == '%')
-		return (1);
-
-	return (0);
-}
-/**
  * _printf - simple implementation of printf
  * @format: string to handle and print
  * Return: number of character
  */
 int _printf(const char *format, ...)
 {
-	int i, len = 0;
+	int i = 0, len = 0;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-	for (i = 0; format[i]; i++)
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (check(format[i]))
-				len += call_f(format[i])(args);
+			
+			len += call_f(format[i])(args);
 		}
 		else
 			len += _putchar(format[i]);
+		i++;
 	}
 	va_end(args);
 	return (len);
