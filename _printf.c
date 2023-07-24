f17a7f3f7cd73ec2cd4da68fe481f2aa853e464d
Surafel-Alayou <surafel2alayou@gmail.com>
2023-07-24 00:28:00 -0700

    Done

--- _printf.c
+++ _printf.c
@@ -1,16 +1,81 @@
-#include "main.h"
-
+#include <stdarg.h>
+#include <unistd.h>
 /**
- * _printf - simple implementation of printf
- * @format: string to handle and print
- * Return: number of character
+ * _putchar - my function
+ * @c: my variable
+ * Return: zero
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+/**
+ * _printf - my next function
+ * @format: my next variable
+ * Return: zero
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+int chars_printed = 0;
+va_list args;
+va_start(args, format);
 
-	va_start(args, format);
-	_vprintf(format, args);
-	va_end(args);
-	return (0);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+	case 'c':{
+		char ch = va_arg(args, int);
+
+		chars_printed += _putchar(ch);
+		break;
+		}
+	case 's':{
+		char *str = va_arg(args, char *);
+
+		while (*str)
+		{
+		chars_printed += _putchar(*str);
+		str++;
+		}
+		break;
+		}
+	case '%':
+		chars_printed += _putchar('%');
+		break;
+	default:
+		break;
+}
+}
+else
+{
+chars_printed += _putchar(*format);
+}
+format++;
+}
+va_end(args);
+return (chars_printed);
+}
+/**
+ * main - my main function
+ *
+ *
+ * Return: zero
+ *
+ */
+int main(void)
+{
+char ch = 'A';
+char *str = "Hello, World!";
+
+
+int chars_printed = _printf("Character: %c\n", ch);
+chars_printed += _printf("String: %s\n", str);
+chars_printed += _printf("Percentage sign: %%\n");
+
+_printf("Total characters printed: %d\n", chars_printed);
+return (0);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,1 @@
 #include "main.h"
-
-/**
-	* _putchar - print character to stdout
-	* @c: character to be printed
-	* Return: 1 on success -1 on fail
-	*/
-int _putchar(char c)
-{
-	return (write(STDIN_FILENO, &c, 1));
-}
